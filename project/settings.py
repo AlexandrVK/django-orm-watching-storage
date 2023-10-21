fffbb9204568d3cdc06e1d6cd37d0dcaf07493ff
AlexandrVK <alexandrvk@gmail.com>
2023-10-21 16:49:25 +0300

    Исправлены имена переменных настройки БД

--- project/settings.py
+++ project/settings.py
@@ -5,11 +5,11 @@ load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.environ['HOST'],
-        'PORT': os.environ['PORT'],
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
     }
 }
 
